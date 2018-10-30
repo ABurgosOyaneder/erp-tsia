039adf3c565ee49ace055d559d6eb0f02c25196c
Felipe Godoy <felipe.godoy@ceinf.cl>
2018-10-29 22:36:38 -0300

    se corrige modelo

--- apps/mod_inventario/models.py
+++ apps/mod_inventario/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=100)
+
+class Producto(models.Model):
+    id_prod = models.AutoField(primary_key=True)
+    id_cat = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=20)
+    cod_barra = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    precio_compra = models.IntegerField()
+    precio_venta = models.IntegerField()
+    stock = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
