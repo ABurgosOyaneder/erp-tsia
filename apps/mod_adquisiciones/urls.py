0ea6c8d31bdf7c5684fe67b1c0c124ca251f23f7
Sebastian Alvarez <sebastian.alvareza@utem.cl>
2018-10-30 17:26:20 -0300

    Creacion de vistas Cobro y adquisiciones |

--- apps/mod_adquisiciones/urls.py
+++ apps/mod_adquisiciones/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 #Aqui van la urls de su modulo
 urlpatterns = [
-    path('adquisiciones/', views.index, name="index_adquisiciones")
+    path('index/', views.index, name="index_adquisiciones"),
+    path('adquisiciones/', views.adquisiciones, name="adquisiciones_adquisiciones"),
+    path('compras/', views.compras, name="compras_adquisiciones"),
 ]
--- erp_tsia/settings.py
+++ erp_tsia/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'erp_tsia',
-        'USER': 'soporte',
-        'PASSWORD': 'tsia2018',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'',
+        'PORT':'',
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 
     }
