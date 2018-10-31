19722bf2e1e439cc5e83bb968b68cb8e70a6e209
SebaCornejoZ <s3b2312@users.noreply.github.com>
2018-10-31 16:04:27 -0300

    Se reqgistran los modelos al admin

--- apps/mod_adquisiciones/admin.py
+++ apps/mod_adquisiciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.mod_adquisiciones.models import Adquisicion , Ingreso , Proveedor , Orden_adq
+
+admin.site.register(Adquisicion)
+admin.site.register(Ingreso)
+admin.site.register(Proveedor)
+admin.site.register(Orden_adq)
