8b96bc9e5b99baebd6c9fa55c2b089a0602114d4
Felipe Godoy <felipe.godoy@ceinf.cl>
2018-10-30 23:31:32 -0300

    se agregan modelos para ventas y migracion inicial

--- apps/mod_venta_cot/models.py
+++ apps/mod_venta_cot/models.py
@@ -8,12 +8,16 @@ class Medio_pago(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=20)
     descuento = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 class Producto_cotizacion(models.Model):
     id = models.AutoField(primary_key=True)
     id_prod = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
     precio_unitario = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 class Cotizacion(models.Model):
     id = models.AutoField(primary_key=True)
@@ -27,3 +31,25 @@ class Cotizacion(models.Model):
     impuestos = models.IntegerField()
     descuento = models.FloatField()
     estado = models.BooleanField()
+
+class Cargo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    valor = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Cargo_venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_cotizacion = models.ForeignKey(Cotizacion, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    total = models.IntegerField()
+    estado = models.CharField(max_length=20)
+    id_cargos = models.ForeignKey(Cargo_venta, on_delete=models.CASCADE)
