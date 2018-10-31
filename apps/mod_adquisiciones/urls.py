95fbad3616250129d8de9fe28e60c9b40c229ad5
SebaCornejoZ <s3b2312@users.noreply.github.com>
2018-10-31 16:31:04 -0300

    Creacion index para adquisiciones

--- apps/mod_adquisiciones/urls.py
+++ apps/mod_adquisiciones/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+from apps.mod_adquisiciones.views import AdquisicionesList
 
 urlpatterns = [
     path('index/', views.index, name="index_adquisiciones"),
-    path('adquisiciones/', views.adquisiciones, name="adquisiciones_adquisiciones"),
+    path('adquisiciones/', AdquisicionesList.as_view(), name="adquisiciones_adquisiciones"),
     path('compras/', views.compras, name="compras_adquisiciones"),
 ]
 
--- apps/mod_adquisiciones/views.py
+++ apps/mod_adquisiciones/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from apps.mod_adquisiciones.models import Adquisicion , Ingreso , Proveedor , Orden_adq
 
 def index(request):
     return render(request,'mod_adquisiciones/index.html')
+
 def adquisiciones(request):
     return render(request,'mod_adquisiciones/adquisiciones.html')
+    
 def compras(request):
     return render(request,'mod_adquisiciones/compras.html')
+
+class AdquisicionesList(ListView):
+	model = Adquisicion
+	template_name = 'mod_adquisiciones/adquisiciones.html'
