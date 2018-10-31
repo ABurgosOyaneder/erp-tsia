a262322b51f295ad857c06c99e52b4f82cc6954c
SebaCornejoZ <s3b2312@users.noreply.github.com>
2018-10-31 16:04:52 -0300

    Creacion de form adquisiciones

---
+++ apps/mod_adquisiciones/form.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from apps.mod_adquisiciones.models import Adquisicion
+
+class AdquisicionForm(forms.ModelForm):
+	class Meta:
+		model = Adquisicion
+
+		fields = {
+			'id_cat',
+			'id_prod',
+			'cantidad',
+			'precio_compra',
+		}
+
+		labels = {
+			'id_cat' : 'Categoria',
+			'id_prod' : 'Productos',
+			'cantidad' : 'Cantidad',
+			'precio_compra' : 'Precio de Compra',
+		}
+
+		widgets = {
+			'id_cat' : forms.Select(attrs={'class':'form-control'}),
+			'id_prod' : forms.CheckboxSelectMultiple(attrs={'class':'form-control'}),
+			'cantidad' : forms.NumberInput(attrs={'class':'form-control'}),
+			'precio_compra' : forms.NumberInput(attrs={'class':'form-control'}),
+		}
