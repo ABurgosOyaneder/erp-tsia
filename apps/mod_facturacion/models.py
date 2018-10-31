a1d7daeec1c8f9694f7b1d7b1e8c15a41f67fd72
Felipe Godoy <felipe.godoy@ceinf.cl>
2018-10-30 23:37:26 -0300

    se agrega modelo y migracion inicial para mod_facturacion

--- apps/mod_facturacion/models.py
+++ apps/mod_facturacion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.mod_venta_cot.models import Venta
 
 # Create your models here.
+
+class Factura(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    vencimiento = models.DateField()
