05f609ad6ff9d28fba31ecd50f5b098756f5a684
RicardoSom <r.orellana.munoz@gmail.com>
2018-11-05 20:03:47 -0300

    Se agregan modelos a admin.py

--- apps/mod_venta_cot/admin.py
+++ apps/mod_venta_cot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.mod_venta_cot.models import Cotizacion , Cargo , Venta , Medio_pago , Cargo_venta , Producto_cotizacion
 
 # Register your models here.
+
+admin.site.register(Cotizacion)
+admin.site.register(Cargo)
+admin.site.register(Venta)
+admin.site.register(Medio_pago)
+admin.site.register(Cargo_venta)
+admin.site.register(Producto_cotizacion)
