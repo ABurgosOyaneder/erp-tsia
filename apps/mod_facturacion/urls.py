9435cc8596d58d330c5016e3423d4d08809888e8
eliezerk8 <gerson.peralta@gmail.com>
2018-11-06 10:02:03 -0300

    se hizo las conexiones del crud

--- apps/mod_facturacion/urls.py
+++ apps/mod_facturacion/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+
 from . import views
 
+app_name = 'facturacion'
 
-#Aqui van la urls de su modulo
+urlpatterns = [
+  path('', views.ListaDeFacturas.as_view(), name='lista_facturas'),
+  path('new', views.CrearFactura.as_view(), name='nueva_factura'),
+  path('edit/<int:pk>', views.ActualizarFactura.as_view(), name='editar_factura'),
+  path('delete/<int:pk>', views.BorrarFactura.as_view(), name='borrar_factura'),
+
+]
