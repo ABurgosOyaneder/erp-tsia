72aca339ce9ee5e7ed4ce3ae1f8f019b27ce65c7
eliezerk8 <gerson.peralta@gmail.com>
2018-11-06 10:01:05 -0300

    agregue el modelo factura y la funcion editar

--- apps/mod_facturacion/models.py
+++ apps/mod_facturacion/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from apps.mod_venta_cot.models import Venta
-
+from django.urls import reverse
+import datetime
 # Create your models here.
 
 class Factura(models.Model):
     id = models.AutoField(primary_key=True)
     id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    iva = models.PositiveSmallIntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     vencimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+    def get_absolute_url(self):
+        return reverse('facturacion:editar_factura', kwargs={'pk': self.pk})
