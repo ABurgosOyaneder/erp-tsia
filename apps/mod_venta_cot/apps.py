ba53b4b4b76329a660d2035d05535776ef67acd3
Felipe Godoy <felipe.godoy@ceinf.cl>
2018-11-06 19:22:23 -0300

    se elimina app no util

--- apps/mod_venta_cot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModVentaCotConfig(AppConfig):
-    name = 'mod_venta_cot'
--- apps/mod_venta_cot/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-
-
-#Aqui van la urls de su modulo
