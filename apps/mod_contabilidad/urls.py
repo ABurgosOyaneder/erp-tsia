fbd2357ab378575f947486778dfa4228020c6ba8
Melody Alhambra <melody.alhambras@utem.cl>
2018-11-12 13:13:08 -0300

    Se hace CRUD de ActivoFijo y URLs

--- apps/mod_contabilidad/urls.py
+++ apps/mod_contabilidad/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+from apps.mod_contabilidad.views import ActivoFijoList, ActivoFijoCreate, ActivoUpdate, ActivoDelete
+
 urlpatterns = [
-    path('contabilidad/', views.index, name="index_contabilidad")
+    path('contabilidad/', views.index, name="index_contabilidad"),
+
+    path('activo/', ActivoFijoList.as_view(), name="contabilidad_activos_fijos"),
+    path('activo/create', ActivoFijoCreate.as_view(), name="contabilidad_activos_fijos_create"),
+    path('activo/update/<int:pk>/', ActivoFijoUpdate.as_view(), name="contabilidad_activos_fijos_update"),
+    path('activo/delete/<int:pk>/', ActivoFijoDelete.as_view(), name="contabilidad_activos_fijos_delete"),
 ]
--- apps/mod_contabilidad/views.py
+++ apps/mod_contabilidad/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView , DeleteView
+from django.urls import reverse_lazy
 
+from apps.mod_contabilidad.models import Contabilidad, ActivoFijo
+from apps.mod_contabilidad.form import ContabilidadForm, ActivoFijoForm
 # Create your views here.
 
 def index(request):
     return render(request,'mod_contabilidad/index.html')
+
+def contabilidad(request):
+    return render(request, 'mod_contabilidad/contabilidad.html')
+
+
+class ActivoFijoCreate(CreateView):
+    model = ActivoFijo
+    form_class = ActivoFijoForm
+    template_name = 'mod_contabilidad/activofijo_form.html'
+    success_url = reverse_lazy('contabilidad_activos_fijos')
+
+class ActivoFijoList(ListView):
+	model = ActivoFijo
+	template_name = 'mod_contabilidad/activofijo_list.html'
+
+
+class ActivoFijoUpdate(UpdateView):
+	model = ActivoFijo
+	form_class = ActivoFijoForm
+	template_name = 'mod_contabilidad/activofijo_form.html'
+	success_url = reverse_lazy('contabilidad_activos_fijos')
+
+class ActivoFijoDelete(DeleteView):
+	model = ActivoFijo
+	form_class = ActivoFijoForm
+	template_name = 'mod_contabilidad/activofijo_delete.html'
+	success_url = reverse_lazy('contabilidad_activos_fijos')
